a77dc8ba6f26e827c19e6cfaffee146b8dbc1070
anasofiperezz <123486693+anasofiperezz@users.noreply.github.com>
2023-05-15 16:40:43 -0600

    Add files via upload

---
+++ products/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', Products.as_view(), name='products'),
+    path('create_product', CreateProduct.as_view(), name='create_product'),
+    path('create_brand', CreateBrand.as_view(), name='create_brand'),
+    path('update/<int:id>', UpdateProduct.as_view(), name='update_product'),
+    path('delete/<pk>', DeleteProduct.as_view(), name='delete_product'),
+    path('delete/<pk>', DeleteBrand.as_view(), name='delete_brand'),
+    path('signin', Signin.as_view(), name='sign_in'),
+    path('signout', Signout.as_view(), name='sign_out')
+
+]
